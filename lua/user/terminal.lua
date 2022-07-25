562ec2c86dc6d26909520f0babcafe05088f586d
M Cooper Healy <m.cooper.healy@gmail.com>
2022-07-25 09:37:14 -0500

    Remove color columns in terminal

--- lua/user/terminal.lua
+++ lua/user/terminal.lua
@@ -1,5 +1,5 @@
 local group = vim.api.nvim_create_augroup("Terminal", { clear = true })
 vim.api.nvim_create_autocmd("TermOpen", {
-    command = "setlocal listchars= nonumber norelativenumber",
-    group = group
-  })
+	command = "setlocal listchars= nonumber norelativenumber cc=",
+	group = group,
+})
