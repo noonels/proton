11eae7ff73f397bd9af07d1889780a8fa273f62c
M Cooper Healy <m.cooper.healy@gmail.com>
2022-07-25 10:01:52 -0500

    Auto install lsp servers

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,6 +3,10 @@ if not status_ok then
   return
 end
 
+require("nvim-lsp-installer").setup {
+  automatic_installation = true
+}
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
@@ -11,12 +15,13 @@ lsp_installer.on_server_ready(function(server)
     capabilities = require("user.lsp.handlers").capabilities,
   }
 
-   if server.name == "jsonls" then
-     local jsonls_opts = require("user.lsp.settings.jsonls")
-     opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-   end
+  if server.name == "jsonls" then
+    local jsonls_opts = require("user.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
 
   -- This setup() function is exactly the same as lspconfig's setup function.
   -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
   server:setup(opts)
 end)
+
